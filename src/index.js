aae000c0c1f192636307b7cb2c7c08781ad78898
orange-circus <72962553+orange-circus@users.noreply.github.com>
2021-01-17 16:46:13 +0900

    修正しました

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import "./styles.css";
+// import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// document.getElementById("app").innerHTML = `
+// <h1>Hello Vanilla!</h1>
+// <div>
+//   We use the same configuration as Parcel to bundle this sandbox, you can find more
+//   info about Parcel
+//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// </div>
+// `;
+
+/**
+ * 三項演算子
+ */
+//ある条件？truthの時:falseの時
+// const val1 = 1 < 0 ? "trueです" : "falseです";
+// console.log(val1);
+// const num = 1300;
+
+// const formatNum = typeof num === 'number' ? num.toLocaleString():'数値を入力してください';
+// console.log(formatNum);
+// const checkSum = (num1,num2)=>{
+//   return num1 + num2 > 100 ? '100を超えています！':'許容範囲内です';
+
+// }
+// console.log(checkSum(500,50));
+
+/**
+ * 論理演算しの本当の意味を知ろう！
+ */
+// 左がfalseとなるときに右を返す
+
+// const num = null;
+// const fee = num || '金額未設定です';
+// console.log(fee);
+
+// &&の左側がtrueなら右を返す
+// const num2 = null;
+// const fee2 = num2 && '何か設定されました';
+// console.log(fee2);
